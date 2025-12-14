3982177c1f13d2645259f2a7019295ceb4d9224e
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2025-12-15 10:41:05 +1100

    Flat minecraft commit YIPPEEEEEEEEEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- local_frame/flat_minecraft/1.0-Alpha/game/src/App.jsx
+++ local_frame/flat_minecraft/1.0-Alpha/game/src/App.jsx
@@ -1,33 +1,137 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import { useImperativeHandle } from 'react'
+import { forwardRef } from 'react'
 import './App.css'
+import arm from '/steve_arm.png'
+import head from '/steve_head.png'
+import body from '/steve.png'
+import leg from '/steve_leg.png'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Player = forwardRef((props, ref)=>{
+  const [head_angle, setHead_angle] = useState(0);
+  const [head_pos, setHead_pos] = useState({ x: 0, y: 0 });
+  const [arm_angle, setArm_angle] = useState(0);
+  const [arm_pos, setArm_pos] = useState({ x: 0, y: 0 });
+  const [body_angle, setBody_angle] = useState(0);
+  const [body_pos, setBody_pos] = useState({ x: 0, y: 0 });
+  const [leg_angle, setLeg_angle] = useState(0);
+  const [leg_pos, setLeg_pos] = useState({ x: 0, y: 0 });
+
+  const update = (x, y) => {
+    setLeg_pos({x: x, y: y-100})
+    setBody_pos({x: x, y: y-200})
+    setHead_pos({x: x-16, y: y-264})
+    setArm_pos({x: x - (34 - Math.cos(68 * Math.PI / 180) * 34), y: y-200 + Math.sin(68 * Math.PI / 180) * 34})
+    setArm_angle(68)
+  }
+
+  useImperativeHandle(ref, () => ({
+    update
+  }))
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={head} style={{
+          position: "absolute",
+          left: head_pos.x,
+          top: head_pos.y,
+          width: "64px",
+          height: "64px",
+          transform: `rotate(${head_angle}deg)`
+        }}/>
+      <br/>
+      <img src={arm} style={{
+          position: "absolute",
+          left: arm_pos.x,
+          top: arm_pos.y,
+          width: "100px",
+          height: "32px",
+          transform: `rotate(${arm_angle}deg)`
+        }}/>
+      <br/>
+      <img src={body} style={{
+          position: "absolute",
+          left: body_pos.x,
+          top: body_pos.y,
+          width: "32px",
+          height: "100px",
+          transform: `rotate(${body_angle}deg)`
+        }}/>
+      <br/>
+      <img src={leg} style={{
+          position: "absolute",
+          left: leg_pos.x,
+          top: leg_pos.y,
+          width: "32px",
+          height: "100px",
+          transform: `rotate(${leg_angle}deg)`
+        }}/>
+    </>
+  )
+})
+
+function Block(type, x, y){
+  return(
+    <>
+      <h1>Block</h1>
+    </>
+  )
+}
+
+function Blocks(){
+  const [blockRenderList, setBlockRenderList] = useState([{type: "oak_planks", key: 0, x: 0, y: 0}])
+  return (
+    <>
+      {blockRenderList.map((block) => {
+        return (
+          <Block key={block.key} type={block.type} x={block.x} y={block.y}/>
+        )
+      })}
+    </>
+  )
+}
+
+function Entity(){
+  return(
+    <>
+      <h1>Entity</h1>
+    </>
+  )
+}
+
+function Entities(){
+  const [entityRenderList, setEntityRenderList] = useState([{id: 0, key: 0, x: 0, y: 0, direction: 0}])
+  return (
+    <>
+      {entityRenderList.map((entity) => {
+        return (
+          <Entity key={entity.key}/>
+        )
+      })}
+    </>
+  )
+}
+
+function Game(){
+  const gateRef = useRef();
+  useEffect(() => {
+    gateRef.current.update(500, 500)
+  }, []);
+  return (
+    <>
+      <Player ref={gateRef}/>
+      <Entities/>
+      <Blocks/>
+    </>
+  )
+}
+
+function App(){
+  return (
+    <>
+      <Game/>
     </>
   )
 }
---
+++ local_frame/flat_minecraft/1.0-Alpha/game/src/minecraftEngine.js
@@ -0,0 +1,47 @@
+const MSPT_ = 50;
+let lastTick = performance.now();
+
+function start20TPSLoop(tickFunction, setFunction, input) {
+  function tick(now) {
+    const MSPT = now - lastTick;
+    if (MSPT >= MSPT_) {
+      lastTick = now - (MSPT % MSPT_);
+      setFunction(tickFunction(input));
+    }
+    requestAnimationFrame(tick);
+  }
+  requestAnimationFrame(tick);
+}
+
+function tick(input){
+    const seed = input[0]
+    let player = input[1]
+    let entity = input[2]
+    let block = input[3]
+    let tick = input[4]
+    let output = [seed]
+    let _
+
+    tick++
+
+    for(p of player){
+        if(p?.action?.place?.slot != undefined && p?.action?.place?.pos?.x != undefined && p?.action?.place?.pos?.y != undefined){
+            for(i of block){
+                if(i.x == p.action.place.pos.x && i.y == p.action.place.pos.y){
+                    _ = block.indexOf(i)
+                    if(_==-1){
+                        block.push(p.inventory.hotbar[p.action.place.slot])
+                    } else {
+                        block[_] = p.inventory.hotbar[p.action.place.slot]
+                    }
+                }
+            }
+        }
+    }
+
+    output.push(player, entity, block, tick)
+    return output;
+}
+
+//input = [seed, player, entity, block]
+//input = [123456789, [{username: "h7777", inventory: {hotbar: [{type: "oak_log", ammount: 1}]}}], [{x: 0, y: 0, type: "arrow"}, {x: 0, y: 0, type: "player"}], [{x: 0, y: -1, type: "oak_plank"}], 0]
