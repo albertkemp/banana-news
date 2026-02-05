38e43198f0a21a2cb127ebfd99a943cbf9ab0a40
h7777v <132132792+h7777v@users.noreply.github.com>
2026-02-05 21:18:58 +1100

    BIG BETA PROGRESS!!!!
    BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!BIG BETA PROGRESS!!!!

--- .beta.cjs
+++ .beta.cjs
@@ -1,3 +1,4 @@
+const cheerio = require("cheerio");
 const path = require('path');
 const fs = require("fs");
 
@@ -11,6 +12,27 @@ module.exports({
         }
         const pageName = path.normalize(inputPath).replace(/^(\.\/|\/)+/, '');
         const raw = fs.readFileSync("./.beta.pages.json", "utf8");
-        return content;
+        const config = JSON.parse(raw);
+        const $ = cheerio.load(content);
+        for(const i of config){
+            if(i?.page == pageName){
+                if(typeof i?.template != "string" && i?.template == undefined)return throwError("The template parameter must be string. ");
+                switch (i?.template) {
+                    case undefined:
+                        break;
+                    case "none":
+                        break;
+                    case "bn":
+                        const templatehtml = fs.readFileSync("./beta_assets/base.htm", "utf8");
+                        const body = $("body").html();
+                        const templated = templatehtml.replace("{{Content}}", body);
+                        $("body").html(templated);
+                        break;
+                    default:
+                        return throwError(`"${i?.template}" was not a template. `);
+                }
+            }
+        }
+        return $.html();
     }
 })
