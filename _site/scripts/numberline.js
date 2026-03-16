59deae0ea66a141767b51e9595013afd57ca8c32
h7777v <132132792+h7777v@users.noreply.github.com>
2026-03-16 15:55:19 +1100

    Re-sync + babel added.
    BabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabelBabel

---
+++ _site/scripts/numberline.js
@@ -0,0 +1,58 @@
+// and , Please ask  before editing or deleting this code
+/*
+[]
+ []
+ []]]
+ []]]]]]
+  []]]]]]]
+   []]]]]]]]
+     []]]]]]]]
+       []]]]]]]]]]
+         []]\]]]]]]]]]]
+           []]]]]]]]]
+|
+|___     ____      ___     ____      ___     ____            ___     ___               __
+|   \   /   |   | /   \   /   |   | /   \   /   |         | /   \   /___\   |  |   |  (__ 
+|___/   \__/ \  |/    |   \__/ \  |/    |   \__/ \        |/    |   \____   \__/\__/   __)
+
+  _____________________________________________________________________________________________________________________________
+ |                                                                                                                             |
+ |   __                                                                            |                        __                 | 
+ |  |  \   __   _   _  |  __            |     _      _ __   __   __|         __    |/     _   _            /__ o   _  __  |    |
+ |  |__/  /__\ / \ | \ | /__\   |  |  | |__  / \   |/ /__\ /  | /  |   | |  (__    |\  | / \ / \ |  |  |   |   | |/  (__ _|__  |
+ |  |     \__  \_/ |_/ | \__    \_/ \_/ |  | \_/   |  \__  \_/ \\_/|   \_/\  __)   | \ |/  | \_/ \_/ \_/   |   | |    __) |_/  |
+ |                 |                                                                                                           |
+ |_____________________________________________________________________________________________________________________________|
+
+
+If you're the sort of person who looks at the source code of webpages, try our challenge:
+
+https://banana-news.github.io/banana/share_this_page
+
+*/
+document.addEventListener("DOMContentLoaded", function() {
+    const numberLine = document.getElementById('number-line');
+
+    const min = -10;
+    const max = 10;
+    const step = 1;
+    const totalTicks = (max - min) / step;
+
+    for (let i = 0; i <= totalTicks; i++) {
+        const value = min + (i * step);
+        const tick = document.createElement('div');
+        tick.className = 'tick';
+        tick.style.left = `${(i / totalTicks) * 100}%`;
+        
+        const label = document.createElement('div');
+        label.className = 'label';
+        label.style.left = `${(i / totalTicks) * 100}%`;
+        label.textContent = value;
+
+        console.log(`Tick ${i}: value=${value}, left=${(i / totalTicks) * 100}%`);
+
+        numberLine.appendChild(tick);
+        numberLine.appendChild(label);
+    }
+});
+
