97ab01190f392ae393625a1238049ca633ec4e81
h7777v <132132792+h7777v@users.noreply.github.com>
2026-03-21 14:50:34 +1100

    Update paper.ts
    Auto div to aspect ratio script.

--- scripts/paper.ts
+++ scripts/paper.ts
@@ -1,1 +1,6 @@
-console.log("abc")
+const TitleDiv = <HTMLDivElement>document.getElementById("title-div");
+
+window.addEventListener("resize", ()=>{
+    const width: number = TitleDiv.getBoundingClientRect().width;
+    TitleDiv.style.height = `${width / 229 * 180}px`;
+});
