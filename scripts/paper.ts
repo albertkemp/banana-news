f95e1ac997d8dbf9d9e033ffbf88a16e75394b05
h7777v <132132792+h7777v@users.noreply.github.com>
2026-03-22 15:50:18 +1100

    Implemented paper.
    Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper. Implemented paper.

--- scripts/paper.ts
+++ scripts/paper.ts
@@ -2,7 +2,10 @@ const TitleDiv = <HTMLDivElement>document.getElementById("title-div");
 const page: { current: string, pages: HTMLElement[] } = {
     current: location.hash.slice(1), 
     pages: [
-        <HTMLDivElement>document.getElementById("main")
+        <HTMLDivElement>document.getElementById("main"), 
+        <HTMLDivElement>document.getElementById("normal"), 
+        <HTMLDivElement>document.getElementById("small"), 
+        <HTMLDivElement>document.getElementById("custom") 
     ]
 };
 
@@ -15,6 +18,7 @@ function updatePageDOM(__page__: string):void{
     for(const i of page.pages){
         i.style.display = "none";
     }
+    location.hash = __page__;
     switch(__page__){
         case "":
             (document.getElementById("main") as HTMLDivElement).style.display = "block";
@@ -22,7 +26,15 @@ function updatePageDOM(__page__: string):void{
         case "normal":
             (document.getElementById("normal") as HTMLDivElement).style.display = "block";
             break;
+        case "small":
+            (document.getElementById("small") as HTMLDivElement).style.display = "block";
+            break;
+        case "custom":
+            (document.getElementById("custom") as HTMLDivElement).style.display = "block";
+            break;
         default:
+            (document.getElementById("main") as HTMLDivElement).style.display = "block";
+            location.hash = "";
             break;
     }
 }
@@ -34,6 +46,8 @@ function updatePage(current: string):void{
 
 function initProductButton(): void{
     (document.getElementById("to-normal") as HTMLElement).onclick = () => updatePage("normal");
+    (document.getElementById("to-small") as HTMLElement).onclick = () => updatePage("small");
+    (document.getElementById("to-custom") as HTMLElement).onclick = () => updatePage("custom");
 }
 
 function init():void{
