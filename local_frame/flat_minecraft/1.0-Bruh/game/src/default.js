e1f32971cdeb11a90ed4fc1bf84b0d337d4c89c8
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2026-04-12 13:35:58 +1000

    FMC joke update #1.0-Bruh #FMC-joke-update #FMC #Big commit

---
+++ local_frame/flat_minecraft/1.0-Bruh/game/src/default.js
@@ -0,0 +1,69 @@
+const uuid =  crypto.randomUUID();
+
+const __default__ = [
+  123456789,
+  [
+    {
+      username: "h7777",
+      uuid: uuid, 
+      inventory: {
+        hotbar: [
+          {
+            type: "bruh",
+            ammount: 1
+          }, 
+          {
+            type: "",
+            ammount: 0
+          }, 
+          {
+            type: "",
+            ammount: 0
+          }, 
+          {
+            type: "",
+            ammount: 0
+          }, 
+          {
+            type: "",
+            ammount: 0
+          }, 
+          {
+            type: "",
+            ammount: 0
+          }, 
+          {
+            type: "",
+            ammount: 0
+          }, 
+          {
+            type: "",
+            ammount: 0
+          }, 
+          {
+            type: "",
+            ammount: 0
+          }
+        ]
+      }, 
+      action: {}, 
+    }
+  ],
+  [
+    {
+      x: 0,
+      y: 0,
+      v: [0, 0],
+      uuid: uuid, 
+      s: 0, 
+      type: "player"
+    }
+  ],
+  [],
+  0, 
+  [0, -1], 
+  0, 
+  true
+]
+
+export { __default__ }
